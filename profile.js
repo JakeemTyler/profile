0c5ab19d69fc091ecbbd2bb48feab486bcee942c
JakeemTyler <89406034+JakeemTyler@users.noreply.github.com>
2022-10-21 17:42:14 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,8 @@
+const establishProfile = ()=>{
+    $("#name").text("Jakeem Tyler");
+    $("#major").text("Computer Graphic Design- Interactive Multimedia");
+    $("#year").text("2023");
+    $("h4").html("<em> USA</em>");
+};
+
+$(document).ready(establishProfile)
